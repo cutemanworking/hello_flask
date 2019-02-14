9f4ce852063ce0680df031a7cbff2ad5fcca03b1
gandalf <gandalfwong@gmail.com>
2019-02-14 22:51:43 +0800

    3.3 - 404(with little problem

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask import request
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
 #    user_agent = request.headers.get('User-Agent')
@@ -11,6 +13,14 @@ def index():
 def user(name):
 #   return '<h1>Hello, {}!</h1>'.format(name)
    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(505)
+def internal_server_error(e):
+    return render_template('500.html'), 500  
 
 if __name__ == '__main__':
     app.run(debug=True)
