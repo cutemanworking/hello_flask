6f62e205101275c72550901f823e448a87ba27a1
gandalf <gandalfwong@gmail.com>
2019-02-16 22:53:03 +0800

    upgrade flask to > 1

--- hello.py
+++ hello.py
@@ -1,18 +1,12 @@
 from flask import Flask, render_template
 from flask import request
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
-@app.route('/')
-def index():
-#    user_agent = request.headers.get('User-Agent')
-#    return '<p>Your browser is {}</p>'.format(user_agent)    
-#    return '<h1>HelloWorld!</h1>'
-   return render_template('index.html')
-@app.route('/user/<name>')
-def user(name):
-#   return '<h1>Hello, {}!</h1>'.format(name)
-   return render_template('user.html', name=name)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -21,6 +15,20 @@ def page_not_found(e):
 @app.errorhandler(505)
 def internal_server_error(e):
     return render_template('500.html'), 500  
+
+
+@app.route('/')
+def index():
+   return render_template('index.html',
+                          current_time=datetime.utcnow())
+@app.route('/user/<name>')
+def user(name):
+   return render_template('user.html', name=name)
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
