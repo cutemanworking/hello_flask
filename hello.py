95f065747c25cc7f64b51d6902914dc3d9c5396b
gandalf <gandalfwong@gmail.com>
2019-02-20 22:21:43 +0800

    4.3

--- hello.py
+++ hello.py
@@ -3,7 +3,15 @@ from flask import request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hardtoguessstring'
+class NameForm(FlaskForm):
+   name = StringField('What is your name?',validators=[DataRequired()])
+   submit = SubmitField('Submit')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -17,10 +25,15 @@ def internal_server_error(e):
     return render_template('500.html'), 500  
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-   return render_template('index.html',
-                          current_time=datetime.utcnow())
+   name = None
+   form = NameForm()
+   if form.validate_on_submit():
+      name = form.name.data
+      form.name.data = ''
+   return render_template('index.html', form=form,name=name,current_time=datetime.utcnow())
+
 @app.route('/user/<name>')
 def user(name):
    return render_template('user.html', name=name)
