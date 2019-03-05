f40da0243d1b8d30ea3b694a449e267e20f2a397
gandalf <gandalfwong@gmail.com>
2019-03-05 10:48:45 +0800

    db function completed

--- hello.py
+++ hello.py
@@ -20,7 +20,7 @@ class Role(db.Model):
    __tablename__='roles'
    id = db.Column(db.Integer,primary_key=True)
    name = db.Column(db.String(64),unique=True)
-   users = db.relationship('User', backref='role'，lazy='dynamic')
+   users = db.relationship('User', backref='role')
    def __repr__(self):
       return '<Role %r>' % self.name
 
@@ -54,18 +54,31 @@ def index():
    form = NameForm()
    if form.validate_on_submit():
 #      name = form.name.data
-      old_name = session.get('name')
-      if old_name is not None and old_name != form.name.data:
-         flash('Looks like you have changed your name!')
+#      old_name = session.get('name')
+      user = User.query.filter_by(username=form.name.data).first()
+      if user is None:
+         user = User(username=form.name.data)
+         db.session.add(user)
+         db.session.commit()
+         session['known'] = False
+      else:
+         session['known'] = True
+#      if old_name is not None and old_name != form.name.data:
+#         flash('Looks like you have changed your name!')
       session['name'] = form.name.data
       return redirect(url_for('index'))
 #      form.name.data = ''
-   return render_template('index.html', form=form,name=session.get('name'),current_time=datetime.utcnow())
+#   return render_template('index.html', form=form,name=session.get('name'),current_time=datetime.utcnow())
+   return render_template('index.html', form=form,name=session.get('name'),known=session.get('known', False),current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
    return render_template('user.html', name=name)
 
+
+@app.shell_context_processor
+def make_shell_context():
+   return dict(db=db, User=User, Role=Role)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)
