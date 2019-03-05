97d0ee465fdb240fc7afb129efa144c9e1824a0a
gandalf <gandalfwong@gmail.com>
2019-03-05 09:57:32 +0800

    addded db

--- hello.py
+++ hello.py
@@ -6,9 +6,32 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hardtoguessstring'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+   __tablename__='roles'
+   id = db.Column(db.Integer,primary_key=True)
+   name = db.Column(db.String(64),unique=True)
+   users = db.relationship('User', backref='role'，lazy='dynamic')
+   def __repr__(self):
+      return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 class NameForm(FlaskForm):
    name = StringField('What is your name?',validators=[DataRequired()])
    submit = SubmitField('Submit')
