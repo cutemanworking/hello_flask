47db0c97b92d4be1b347b92b8c9ff84eb32f2337
gandalf <gandalfwong@gmail.com>
2019-03-06 19:12:22 +0800

    add db migration

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,6 +16,7 @@ app.config['SECRET_KEY'] = 'hardtoguessstring'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 class Role(db.Model):
    __tablename__='roles'
